12718939a1fd78b902b5eea95b3ab3d64ca47ea4
Marcus Mennemeier <marcus@mennemeier.de>
2014-06-13 18:19:01 +0200

    depend make rule on jade template

--- htmldoc.mk
+++ htmldoc.mk
@@ -30,7 +30,7 @@ htmldoc/clean:
 $(TOOLS)/htmldoc/node_modules: $(TOOLS)/htmldoc/package.json
 	cd $(TOOLS)/htmldoc && npm install && touch $(TOOLS)/htmldoc/node_modules
 
-$(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component-is-build $(TOOLS)/htmldoc/node_modules
+$(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component-is-build $(TOOLS)/htmldoc/node_modules $(TOOLS)/htmldoc/htmldoc.jade
 	@rm -rf $(HTMLDOC)
 	@mkdir -p "$(HTMLDOC)"
 	tar -c --exclude component-is-build --directory "$(<D)" . | tar -x --directory "$(HTMLDOC)"
