62882a4a828a6fade405955245c87a559eadb118
Marcus Mennemeier <m.mennemeier@global-communication.de>
2015-03-11 15:23:27 +0100

    add clean/npm_tmp target

--- npm-install.mk
+++ npm-install.mk
@@ -10,15 +10,17 @@ endif
 
 # flag: NPM_PRODUCTION_DEPENDENCIES_ONLY=1 only include npm production dependencies
 ifdef ($(NPM_PRODUCTION_DEPENDENCIES_ONLY),)
-	NPM_FLAGS += --production
+	override NPM_FLAGS += --production
 endif
+
+CLEAN_NPM_TMP = rm -rf "$(shell npm config get tmp)/npm-*" 2> /dev/null || exit 0
 
 node_modules: $(NODE_MODULES_DEP)
 
 $(NODE_MODULES_DEP): package.json
 	npm install $(NPM_FLAGS)
 	@cat node_modules/*/package.json | shasum > $@
-	@rm -rf "$(shell npm config get tmp)/npm-*" 2> /dev/null || exit 0
+	@$(CLEAN_NPM_TMP)
 
 npm-shrinkwrap.json: node_modules
 	npm prune
@@ -31,4 +33,8 @@ clean: clean/node_modules
 clean/node_modules:
 	rm -rf node_modules
 
-.PHONY: clean clean/node_modules
+clean/npm_tmp:
+	$(CLEAN_NPM_TMP)
+
+.PHONY: clean clean/node_modules clean/npm_tmp
+
