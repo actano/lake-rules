6d2dc38ecedf07833bc2992688af6a2ecdf761b5
Marcus Mennemeier <m.mennemeier@global-communication.de>
2015-03-11 13:52:01 +0100

    delete less from npm temp, add support for NPM_PRODUCTION_DEPENDENCIES_ONLY

--- npm-install.mk
+++ npm-install.mk
@@ -8,12 +8,17 @@ $(shell echo $(NEW_SUM) > $(NODE_MODULES_DEP))
 endif
 endif
 
+# flag: NPM_PRODUCTION_DEPENDENCIES_ONLY=1 only include npm production dependencies
+ifdef ($(NPM_PRODUCTION_DEPENDENCIES_ONLY),)
+	NPM_FLAGS += --production
+endif
+
 node_modules: $(NODE_MODULES_DEP)
 
 $(NODE_MODULES_DEP): package.json
-	npm install
+	npm install $(NPM_FLAGS)
 	@cat node_modules/*/package.json | shasum > $@
-	@rm -rf "$(shell npm config get tmp)" 2> /dev/null || exit 0
+	@rm -rf "$(shell npm config get tmp)/npm-*" 2> /dev/null || exit 0
 
 npm-shrinkwrap.json: node_modules
 	npm prune
